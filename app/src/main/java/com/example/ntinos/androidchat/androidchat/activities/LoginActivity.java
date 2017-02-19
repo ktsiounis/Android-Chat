13584c10999f68a2b45d6949b99b7fc99e295a75
ktsiounis <konstantinos.tsiounis@hotmail.com>
2017-02-19 14:37:45 +0200

    Got rid off some useless variables

--- app/src/main/java/com/example/ntinos/androidchat/androidchat/activities/LoginActivity.java
+++ app/src/main/java/com/example/ntinos/androidchat/androidchat/activities/LoginActivity.java
@@ -59,14 +59,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -251,12 +243,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
